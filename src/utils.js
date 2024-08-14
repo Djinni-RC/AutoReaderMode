3a3ddde6aed91e94f043b30b983a3c9c7485d18c
BuildTools <unconfigured@null.spigotmc.org>
2024-08-14 14:01:41 +0200

    added basic check for base url

--- src/utils.js
+++ src/utils.js
@@ -5,14 +5,21 @@ function convertUrl(originalUrl) {
         }
 
         const urlObject = new URL(originalUrl);
-        const domain = urlObject.hostname;
-        const baseUrl = `read://https_${domain}/?url=`;
-        const encodedUrl = encodeURIComponent(originalUrl);
-        const readerUrl = baseUrl + encodedUrl;
+        var isBase = urlObject.toString().match(/^https{0,1}\:\/\/\w+\.\w+\/{0,1}$/gim);
+        if(null == isBase){isBase = false}else{isBase = true}
+        if(isBase){
 
-        console.log(`Converted ${originalUrl} to ${readerUrl}`);
-
-        return readerUrl;
+            const domain = urlObject.hostname;
+            const baseUrl = `read://https_${domain}/?url=`;
+            const encodedUrl = encodeURIComponent(originalUrl);
+            const readerUrl = baseUrl + encodedUrl;
+            
+            console.log(`Converted ${originalUrl} to ${readerUrl}`);
+            
+            return readerUrl;
+        }else{
+            return originalUrl
+        }
     } catch (e) {
         console.error('Invalid URL:', originalUrl);
         return null;
