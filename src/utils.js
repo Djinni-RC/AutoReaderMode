13e4d6197e1e1969fd274d4cd4d59d2da8074519
BuildTools <unconfigured@null.spigotmc.org>
2024-08-14 14:05:50 +0200

    removed bloat

--- src/utils.js
+++ src/utils.js
@@ -1,22 +1,9 @@
 function convertUrl(originalUrl) {
     try {
-        if (originalUrl.startsWith('read://')) {
-            return originalUrl;
-        }
-
-        const urlObject = new URL(originalUrl);
-        var isBase = urlObject.toString().match(/^https{0,1}\:\/\/\w+\.\w+\/{0,1}$/gim);
+        var isBase = originalUrl.toString().match(/^https{0,1}\:\/\/\w+\.\w+\/{0,1}$/gim);
         if(null == isBase){isBase = false}else{isBase = true}
         if(isBase){
-
-            const domain = urlObject.hostname;
-            const baseUrl = `read://https_${domain}/?url=`;
-            const encodedUrl = encodeURIComponent(originalUrl);
-            const readerUrl = baseUrl + encodedUrl;
-            
-            console.log(`Converted ${originalUrl} to ${readerUrl}`);
-            
-            return readerUrl;
+            return `read://${originalUrl}`;
         }else{
             return originalUrl
         }
